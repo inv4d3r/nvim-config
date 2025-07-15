e042e8129984c068d4e37f479cbeec2ce53060b1
Mariusz Zalewski <mariusz.zalewski@tty.host>
2025-07-15 13:59:53 +0200

    feat: add render-markdown for inline preview

--- lua/plugins/documentation.lua
+++ lua/plugins/documentation.lua
@@ -26,6 +26,21 @@ return {
       vim.keymap.set("n", "<leader>mb", "<Plug>MarkdownPreviewToggle", { silent = true })
     end
   },
+  -- render-markdown
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    -- opts = { enabled = false },
+    config = function()
+      local rm = require('render-markdown')
+      rm.setup({ enabled = false })
+      vim.keymap.set("n", "<leader>rm", "<cmd>RenderMarkdown toggle<CR>", { silent = true })
+    end
+  },
   -- glow
   {
     "ellisonleao/glow.nvim",
