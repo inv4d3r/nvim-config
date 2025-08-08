bf95002f5238743976b38f1cc66dae15d3c4e1d5
Mariusz Zalewski <mariusz.zalewski@tty.host>
2025-08-08 12:48:06 +0200

    feat: add treesitter context and motions

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -66,6 +66,6 @@ vim.keymap.set('n', '<leader>te', '<cmd>tabe term://bash<cr>')
 -- see `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>gof', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '[D', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']D', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<leader>gq', vim.diagnostic.setloclist, opts)
