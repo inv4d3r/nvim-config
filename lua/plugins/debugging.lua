0513585734828cdf8db2d19037c8d211191aa581
Mariusz Zalewski <mariusz.zalewski@tty.host>
2026-04-17 15:28:49 +0200

    feat(python) add pytest runners with no coverage to DAP

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -25,26 +25,26 @@ return {
         dap.toggle_breakpoint(condition, hit_condition, log_message)
       end
 
-      vim.keymap.set("n", "<F3>", dap.disconnect)
-      vim.keymap.set("n", "<F4>", dap.restart)
-      vim.keymap.set("n", "<F5>", continue)
-      vim.keymap.set("n", "<F6>", dap.pause)
-      vim.keymap.set("n", "<F7>", dap.terminate)
+      vim.keymap.set("n", "<F3>", dap.disconnect, { desc = "DAP: Disconnect" })
+      vim.keymap.set("n", "<F4>", dap.restart, { desc = "DAP: Restart" })
+      vim.keymap.set("n", "<F5>", continue, { desc = "DAP: Continue" })
+      vim.keymap.set("n", "<F6>", dap.pause, { desc = "DAP: Pause" })
+      vim.keymap.set("n", "<F7>", dap.terminate, { desc = "DAP: Terminate" })
       -- function breakpoint not yet supported
       --vim.keymap.set("n", "<F8>", dap.?)
-      vim.keymap.set("n", "<leader><F8>", dap.run_to_cursor)
-      vim.keymap.set("n", "<F9>", dap.toggle_breakpoint)
-      vim.keymap.set("n", "<leader><F9>", condition_breakpoint)
-      vim.keymap.set("n", "<F10>", dap.step_over)
-      vim.keymap.set("n", "<F11>", dap.step_into)
-      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set("n", "<leader><F8>", dap.run_to_cursor, { desc = "DAP: Run to cursor" })
+      vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "DAP: Toggle breakpoint" })
+      vim.keymap.set("n", "<leader><F9>", condition_breakpoint, { desc = "DAP: Conditional breakpoint" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP: Step over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP: Step into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP: Step out" })
 
-      vim.keymap.set("n", "<leader>dbl", dap.list_breakpoints)
-      vim.keymap.set("n", "<leader>dbc", dap.clear_breakpoints)
-      vim.keymap.set("n", "<leader>dd", dap.down)
-      vim.keymap.set("n", "<leader>du", dap.up)
-      vim.keymap.set("n", "<leader>dr", dap.repl.open)
-      vim.keymap.set("n", "<leader>dl", dap.run_last)
+      vim.keymap.set("n", "<leader>dbl", dap.list_breakpoints, { desc = "DAP: List breakpoints" })
+      vim.keymap.set("n", "<leader>dbc", dap.clear_breakpoints, { desc = "DAP: Clear breakpoints" })
+      vim.keymap.set("n", "<leader>dd", dap.down, { desc = "DAP: Down frame" })
+      vim.keymap.set("n", "<leader>du", dap.up, { desc = "DAP: Up frame" })
+      vim.keymap.set("n", "<leader>do", dap.repl.open, { desc = "DAP: Open REPL" })
+      vim.keymap.set("n", "<leader>dr", dap.run_last, { desc = "DAP: Run last" })
 
       dap.adapters.cppdbg = {
         id = 'cppdbg',
@@ -167,7 +167,22 @@ return {
       local python_path = get_python_path()
       local dap = require("dap")
       local dap_python = require("dap-python")
-      dap_python.setup(python_path)
+
+      local test_runners = dap_python.test_runners
+      -- `test_runners` is a table. The keys are the runner names like `unittest` or `pytest`.
+      -- The value is a function that takes two arguments:
+      -- The classnames and a methodname
+      -- The function must return a module name and the arguments passed to the module as list.
+
+      ---@param classnames string[]
+      ---@param methodname string?
+      test_runners.pytest_no_cov = function(classnames, methodname)
+        local module, args = test_runners.pytest(classnames, methodname)
+        table.insert(args, "--no-cov")
+        return module, args
+      end
+
+      dap_python.setup()
       table.insert(dap.configurations.python, {
         name = "pytest: current file",
         type = "python",
@@ -175,11 +190,15 @@ return {
         module = "pytest",
         args = {
           "${file}",
+          "--no-cov"
         },
         console = "integratedTerminal",
       })
-      vim.keymap.set("n", "<leader>dm", dap_python.test_method)
-      vim.keymap.set("n", "<leader>dc", dap_python.test_class)
+      -- vim.keymap.set("n", "<leader>dm", dap_python.test_method)
+      vim.keymap.set("n", "<leader>dm", function() dap_python.test_method({ test_runner = "pytest_no_cov" }) end,
+        { desc = "DAP Python: Test method" })
+      vim.keymap.set("n", "<leader>dc", function() dap_python.test_class({ test_runner = "pytest_no_cov" }) end,
+        { desc = "DAP Python: Test class" })
     end,
   },
   {
@@ -200,10 +219,10 @@ return {
       end
 
       -- toggle (in case not closed automatically using event)
-      vim.keymap.set({ "n" }, "<leader>dt", dapui.toggle)
+      vim.keymap.set({ "n" }, "<leader>dt", dapui.toggle, { desc = "DAP UI: Toggle" })
 
       -- evaluate expression under cursor or visually selected
-      vim.keymap.set({ "n", "v" }, "<leader>e", dapui.eval)
+      vim.keymap.set({ "n", "v" }, "<leader>de", dapui.eval, { desc = "DAP UI: Evaluate" })
     end,
   }
 }
